55f8661c9337b180bdf31c1d5c1b3dcd34fe833c
Darina Goldin <darina@dojomadness.com>
2020-03-30 11:33:46 +0200

    Add kaggle download script

---
+++ load_kaggle_data.py
@@ -0,0 +1,24 @@
+# get the Corona tweet dataset from Kaggle
+
+# Getting the token: source https://www.kaggle.com/docs/api#interacting-with-datasets
+# In order to use the Kaggle’s public API, you must first authenticate using an API token. From the site header, click
+# on your user profile picture, then on “My Account” from the dropdown menu. This will take you to your account settings
+# at https://www.kaggle.com/account. Scroll down to the section of the page labelled API:
+#
+# To create a new token, click on the “Create New API Token” button. This will download a fresh authentication token
+# onto your machine.
+#
+# If you are using the Kaggle CLI tool, the tool will look for this token at ~/.kaggle/kaggle.json on Linux, OSX, and
+# other UNIX-based operating systems, and at C:\Users<Windows-username>.kaggle\kaggle.json on Windows. If the token is
+# not there, an error will be raised. Hence, once you’ve downloaded the token, you should move it from your Downloads
+# folder to this folder.
+#
+# If you are using the Kaggle API directly, where you keep the token doesn’t matter, so long as you are able to provide
+# your credentials at runtime.
+
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+api = KaggleApi()
+api.authenticate()
+files = api.dataset_download_files('smid80/coronavirus-covid19-tweets', unzip=True, path='data/', quiet=False)
