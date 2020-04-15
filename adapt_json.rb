2f9000fbe914a904d10c91f860d4501e7802abfe
William Bode <william.bode@gmx.de>
2020-04-15 12:44:13 +0200

    fixed fucked up things from merge conflict

--- adapt_json.rb
+++ adapt_json.rb
@@ -1,5 +1,7 @@
-file_name = "networkx_graph_data.json"
-text = File.read(file_name)
-text = text.gsub("'", '"')
-text = text.gsub(': False', ': "False"')
-File.open(file_name, "w") { |file| file.puts text}
+file_names = ["networkx_graph_data.json", "networkx_small_graph_data.json"]
+file_names.each do |file_name|
+	text = File.read(file_name)
+	text = text.gsub("'", '"')
+	text = text.gsub(': False', ': "False"')
+	File.open(file_name, "w") { |file| file.puts text}
+end
